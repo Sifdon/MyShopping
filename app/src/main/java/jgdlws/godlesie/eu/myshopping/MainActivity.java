56ca08528259b2b26c6bc350ef3224fc35d3df0c
jgdlws <j.godlesie@gmail.com>
2018-02-28 20:01:51 +0100

    Initial commit

---
+++ app/src/main/java/jgdlws/godlesie/eu/myshopping/MainActivity.java
@@ -0,0 +1,13 @@
+package jgdlws.godlesie.eu.myshopping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
